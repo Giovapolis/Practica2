4a263de6d054cbc53eb6d5b1aff496d5a9b9ef2e
Giovax <giovapolis@gmail.com>
2018-03-09 18:17:26 -0600

    Ventana

--- src/practica2/recibos.java
+++ src/practica2/recibos.java
@@ -35,7 +35,7 @@ public class recibos extends javax.swing.JFrame {
         jButton1 = new javax.swing.JButton();
         jRadioButton1 = new javax.swing.JRadioButton();
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
 
         jPanel1.setBackground(new java.awt.Color(255, 255, 255));
 
